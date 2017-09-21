dd031de12e550c8bdaccdbfc3864a76ce0792589
Tom Broucke <tombroucke@Toms-iMac.local>
2017-09-21 13:23:49 +0200

    initial commit

---
+++ src/tombroucke/WpSyncPosts.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TomBroucke;
+
+class WpSyncPosts {
+	function __construct(){
+		echo "ok";
+	}
+}
---
+++ tests/test.php
@@ -0,0 +1,8 @@
+<?php
+
+// Autoload files using the Composer autoloader.
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use TomBroucke\WpSyncPosts;
+
+new WpSyncPosts();
